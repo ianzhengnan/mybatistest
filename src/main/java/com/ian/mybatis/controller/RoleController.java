8f6ad33e3a0a164f0d4871722927a81e0c49b526
Ian Zheng <ian.zheng@sap.com>
2018-05-18 16:25:40 +0800

    switch to eclipse

--- src/main/java/com/ian/mybatis/controller/RoleController.java
+++ src/main/java/com/ian/mybatis/controller/RoleController.java
@@ -2,8 +2,12 @@ package com.ian.mybatis.controller;
 
 import com.ian.mybatis.po.Role;
 import com.ian.mybatis.service.RoleService;
+
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,5 +26,16 @@ public class RoleController {
         long end = System.currentTimeMillis();
         System.err.println("getRole spend time: " + (end - start));
         return role;
+    }
+    
+    @GetMapping("/test")
+    @ResponseBody
+    public Role getRole() {
+    	Role role = new Role();
+    	role.setId(12345L);
+    	role.setRoleName("郑楠");
+    	role.setCreateDate(new Date());
+    	role.setNote("This is for testing");
+    	return role;
     }
 }
